88d228dc62b2bc1a7aa2c78a9534dd00a0dfdb46
jzhang1223 <zhang.just@husky.neu.edu>
2020-05-04 00:08:20 -0400

    Setup Tetromino class and fix height issue

--- Tetris.js
+++ Tetris.js
@@ -15,28 +15,23 @@ function setup() {
   console.log(canvas.width);
   console.log(canvas.height);
 
-  game = new TetrisGame(10, 24);
+  game = new TetrisGame(10, 24, canvas);
 
   game.draw_board();
 }
 
 class TetrisGame {
 
-  constructor(width, height) {
+  constructor(width, height, canvas) {
     this.width = width;
-    this.height = height;
+    // Add 2 invisible rows for spawning tetromino
+    this.height = height + 2;
     this.board = new Array(this.width);
     for(var i = 0; i < this.height; i++) {
       this.board[i] = new Array(height);
     }
 
-    var canvas = document.getElementById("tetris");
-
-    // console.log(canvas.height);
-    // console.log(this.height);
-    // console.log(canvas.height / this.height);
-    // this.block_size = canvas.height / this.height;
-    this.block_size = 27;
+    this.block_size = canvas.height / this.height;
   }
 
   print_board() {
@@ -49,7 +44,7 @@ class TetrisGame {
 
   draw_board() {
     for(var i = 0; i < this.width; i++) {
-      for(var j = 0; j < this.height; j++) {
+      for(var j = 2; j < this.height; j++) {
         this.draw_block({x: i, y: j, value: this.board[i][j]});
       }
     }
@@ -64,9 +59,66 @@ class TetrisGame {
     if(block.value === undefined) {
       context.beginPath();
       context.rect(block.x * this.block_size + offset, block.y * this.block_size, this.block_size, this.block_size);
+      context.fillText(block.y, block.x * this.block_size + offset, block.y * this.block_size + this.block_size, this.block_size);
       context.stroke();
     }
   }
 }
 
-var game = new TetrisGame(10, 24);
+class Tetromino {
+
+  constructor(shape) {
+    switch(shape) {
+      case shapes.I:
+        this.squares = [3, 5, 6];
+        break;
+      case shapes.J:
+        this.squares = [0, 3, 5];
+        break;
+      case shapes.L:
+        this.squares = [2, 3, 5];
+        break;
+      case shapes.O:
+        this.squares = [1, 2, 5];
+        break;
+      case shapes.S:
+        this.squares = [1, 2, 3];
+        break;
+      case shapes.Z:
+        this.squares = [0, 1, 5];
+        break;
+      case shapes.T:
+        this.squares = [1, 3, 5];
+        break;
+      default:
+        console.log("INVALID SHAPE GIVEN");
+    }
+
+    this.coordinates = make_coordinate_list(this.squares);
+  }
+}
+
+const shapes = {
+  I: "I",
+  J: "J",
+  L: "L",
+  O: "O",
+  S: "S",
+  Z: "Z",
+  T: "T"
+}
+
+function make_coordinate_list(coordinates) {
+  var result = [make_coordinate(1,1)];
+  for(var i = 0; i < coordinates.length; i++) {
+    if(coordinates[i] < 3) {
+      result.append(make_coordinate(coordinates[i], 0));
+    } else {
+      result.append(make_coordinate(coordinates[i] - 3, 1))
+    }
+  }
+}
+
+function make_coordinate(x, y) {
+  return {x: x, y: y};
+}
